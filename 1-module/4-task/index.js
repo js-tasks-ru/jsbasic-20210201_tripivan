ec550566670ed27f9e3bf8b4f418d766294e9a2d
Ivan Tripolyev <it000309@gmail.com>
2021-02-06 16:33:32 +0300

    1-module 4-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,10 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase();
+  if (str.includes("1xbet") || str.includes("xxx")) {
+    return true;
+  }
+
+  return false;
 }
